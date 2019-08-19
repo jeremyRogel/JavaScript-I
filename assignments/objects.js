dc6f8290d4dfaa60a2b5096ea9f3a9e5c6135dd8
System Administrator <root@Jeremys-MBP.lan1>
2019-08-19 17:42:18 -0400

    completed project for the day

--- assignments/objects.js
+++ assignments/objects.js
@@ -19,19 +19,58 @@ const example = {
 
 // Write your intern objects here:
 
+const Mitzi = {
+  id: 1,
+  email: "mmelloy0@psu.edu",
+  name: "Mitzi",
+  gender: "F",
+}
+
+const Kennan = {
+  id: 2,
+  email: "kdiben1@tinypic.com",
+  name: "Kennan",
+  gender: "M",
+  speak: function() {
+    console.log("Hello, my name is Kennan!");
+  }
+}
+const Keven = {
+  id: 3,
+  email: "kmummery2@wikimedia.org",
+  name: "Keven",
+  gender: "M",
+}
+const Gannie = {
+  id: 4,
+  email: "gmartinson3@illinois.edu",
+  name: "Gannie",
+  gender: "M",
+}
+const Antionetta = {
+  id: 5,
+  email: "adaine5@samsung.com",
+  name: "Antionetta",
+  gender: "F",
+  math: function multiplyNums(num1, num2) {
+    return num1 * num2
+  }
+}
+
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
-
+console.log(Mitzi.name)
 // Kennan's ID
-
+console.log(Kennan.id)
 // Keven's email
-
+console.log(Keven.email)
 // Gannie's name
-
+console.log(Gannie.name)
 // Antonietta's Gender
+console.log(Antionetta.gender)
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
